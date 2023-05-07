6b0c8730104f20314600a4ba40377cc99007bf2a
sample-tayo <103777801+sample-tayo@users.noreply.github.com>
2023-05-07 18:00:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const header = document.querySelector("h2");
 const strikerBtn = document.querySelector("#striker-btn");
 const midfielderBtn = document.querySelector("#midfielder-btn");
 const defenderBtn = document.querySelector("#defender-btn");
+const goldenGloveBtn = document.querySelector("#golden-glove-btn");
 // displaying playername in another div
 const playerName = document.querySelector("#playername"); // the input field where the player name is inputted
 const playerNameDisplay = document.querySelector("#playerNameDisplay"); // the div where the player name will be displayed
@@ -14,8 +15,12 @@ const motmInput = document.querySelector("#motm");
 const yellowCardsInput = document.querySelector("#yellow-cards");
 const redCardsInput = document.querySelector("#red-cards");
 const cleanSheets = document.querySelector("#cleansheets");
+const cleanSheetLabel = document.getElementById("cleansheets-label");
+const goalsInputLabel = document.getElementById("goalsInput-label");
+const assistInputLabel = document.getElementById("assistInput-label");
 const numOfTrophies = document.querySelector("#trophies");
 const averageRating = document.querySelector("#avg-rating");
+const avgRatingLabel = document.querySelector("#avgRating-label");
 // calculate
 const calculateButton = document.querySelector("#calculate-button");
 const totalGoals = document.querySelector("#totalgoals");
@@ -25,7 +30,7 @@ const hidden = document.querySelector(".hidden");
 
 let playerType = "";
 // Add event listeners to the player type buttons
-strikerBtn.addEventListener("click", function () {
+strikerBtn.addEventListener("click", function (event) {
   event.preventDefault(); // Prevent form from submitting and refreshing the page
 
   hidden.classList.remove("hidden");
@@ -37,9 +42,12 @@ strikerBtn.addEventListener("click", function () {
   playerType = "Striker";
   // Hide the clean sheets input
   cleanSheets.style.display = "none";
+  cleanSheetLabel.style.display = "none";
+  averageRating.style.display = "none";
+  avgRatingLabel.style.display = "none";
 });
 
-midfielderBtn.addEventListener("click", function () {
+midfielderBtn.addEventListener("click", function (event) {
   event.preventDefault(); // Prevent form from submitting and refreshing the page
 
   hidden.classList.remove("hidden");
@@ -51,10 +59,11 @@ midfielderBtn.addEventListener("click", function () {
 
   playerType = "midfielder";
   // Show the clean sheets input
-  cleanSheets.style.display = "block";
+  cleanSheets.style.display = "none";
+  cleanSheetLabel.style.display = "none";
 });
 
-defenderBtn.addEventListener("click", function () {
+defenderBtn.addEventListener("click", function (event) {
   event.preventDefault(); // Prevent form from submitting and refreshing the page
 
   hidden.classList.remove("hidden");
@@ -67,7 +76,29 @@ defenderBtn.addEventListener("click", function () {
 
   playerType = "defender";
   // Show the clean sheets input
+  cleanSheetLabel.style.display = "inline";
   cleanSheets.style.display = "block";
+});
+
+goldenGloveBtn.addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent form from submitting and refreshing the page
+
+  hidden.classList.remove("hidden");
+
+  // Show the player name output
+  const text = playerName.value;
+  header.textContent = `Keeper: ${text}`;
+
+  playerName.value = ""; // Clear input field
+
+  playerType = "keeper";
+  // Show the clean sheets input
+  cleanSheetLabel.style.display = "inline";
+  cleanSheets.style.display = "block";
+  goalsInput.style.display = "none";
+  goalsInputLabel.style.display = "none";
+  assistsInput.style.display = "none";
+  assistInputLabel.style.display = "none";
 });
 
 calculateButton.addEventListener("click", () => {
@@ -82,24 +113,63 @@ calculateButton.addEventListener("click", () => {
   let motm = Number(motmInput.value) * (playerType === "defender" ? 4 : 3);
   let yellowCards =
     Number(yellowCardsInput.value) *
-    (playerType === "defender" || playerType === "midfielder" ? -1 : -2);
+    (playerType === "defender" || playerType === "midfielder"
+      ? -1
+      : playerType === "keeper"
+      ? -1
+      : -2);
   let redCards =
     Number(redCardsInput.value) *
-    (playerType === "defender" || playerType === "midfielder" ? -2 : -3);
+    (playerType === "defender" || playerType === "midfielder"
+      ? -2
+      : playerType === "keeper"
+      ? -2
+      : -3);
   const trophies = Number(numOfTrophies.value) * 6;
   let totalCleanSheets =
     Number(cleanSheets.value) *
-    (playerType === "defender" ? 2 : playerType === "midfielder" ? 1 : 0);
+    (playerType === "defender" ? 2 : playerType === "keeper" ? 3 : 0);
 
-  let avgRatingPoints = 0;
   const avgRating = Number(averageRating.value);
-  if (avgRating >= 6.0 && avgRating < 7.0) {
+  let avgRatingPoints = 0;
+  if (
+    avgRating >= 6.0 &&
+    avgRating < 7.0 &&
+    (playerType === "keeper" ||
+      playerType === "defender" ||
+      playerType === "striker" ||
+      playerType === "midfielder")
+  ) {
     avgRatingPoints = 1;
-  } else if (avgRating >= 7.0 && avgRating < 8.0) {
+  } else if (
+    avgRating >= 7.1 &&
+    avgRating < 8.0 &&
+    (playerType === "keeper" || playerType === "defender")
+  ) {
     avgRatingPoints = 3;
-  } else if (avgRating >= 8.0 && avgRating <= 9.5) {
+  } else if (
+    avgRating >= 7.1 &&
+    avgRating < 8.0 &&
+    (playerType === "striker" || playerType === "midfielder")
+  ) {
+    avgRatingPoints = 2;
+  } else if (
+    avgRating >= 8.1 &&
+    avgRating <= 9.5 &&
+    (playerType === "keeper" || playerType === "defender")
+  ) {
     avgRatingPoints = 4;
+  } else if (
+    avgRating >= 8.1 &&
+    avgRating <= 9.5 &&
+    (playerType === "striker" || playerType === "midfielder")
+  ) {
+    avgRatingPoints = 3;
   }
+
+  console.log(avgRatingPoints);
+  console.log("playerType:", playerType);
+  console.log("avgRating:", avgRating);
 
   let totalScore =
     goals +
